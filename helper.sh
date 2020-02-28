500c68e6f339785da1653894776cd89beab5a323
DoMeLe <1812020+djdomi@users.noreply.github.com>
2020-02-28 12:56:25 +0100

    Update helper.sh

--- helper.sh
+++ helper.sh
@@ -1,6 +1,7 @@
 domains="$1"
 for domain in $domains; do
-        echo "mach ${domain}"
+        clear
+        echo "Inserting key for: ${domain}"
         for key in `ls $2/key/K$domain.*.key`
         do
                 echo "\$include $key" >> $2"${domain}"
